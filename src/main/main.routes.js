76e6a243049a8a3d91085ff25a164347c8a73e5b
Filipe Waitman <filwaitman@gmail.com>
2017-03-09 10:38:10 -0300

    WIP

--- src/main/main.routes.js
+++ src/main/main.routes.js
@@ -51,25 +51,34 @@
                 ],
                 itemsData: [
                     'EventService', '$stateParams', '$window', function(EventService, $stateParams, $window) {
-                        $window.timeline_json = {
-                            'events': [
-                                {
+                        return EventService.getItems($stateParams.objectType, $stateParams.objectId).then(function(data){
+                            var events = [];
+                            var event_idx;
+
+                            for (event_idx in data.results){
+                                var event = data.results[event_idx];
+                                var timestamp = new Date(event.timestamp);
+
+                                var event_object = {
                                     'start_date': {
-                                        'year': 2017,
-                                        'month': 1,
-                                        'day': 1,
-                                        'hour': 12,
-                                        'minute': 12,
-                                        'second': 12,
+                                        'year': timestamp.getFullYear(),
+                                        'month': timestamp.getMonth(),
+                                        'day': timestamp.getDay(),
+                                        'hour': timestamp.getHours(),
+                                        'minute': timestamp.getMinutes(),
+                                        'second': timestamp.getSeconds()
                                    },
                                     'text': {
-                                        'headline': 'Gone',
-                                        'text': 'Died',
-                                    },
-                                },
-                            ]
-                        }
-                        return EventService.getItems($stateParams.objectType, $stateParams.objectId);
+                                        'headline': event.message,
+                                        'text': event.message
+                                    }
+                                }
+                                events.push(event_object);
+                            }
+
+                            $window.timeline_json = {'events': events}
+                            return data;
+                        });
                     }
                 ],
 
